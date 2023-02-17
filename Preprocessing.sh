ee67670c1a2c4ab4845cf9886bb9539e33b9d844
YOON-SEOB KIM <38550028+Yoon-Seob-Kim@users.noreply.github.com>
2023-02-17 10:32:58 +0900

    Add files via upload

--- Preprocessing.sh
+++ Preprocessing.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #SBATCH -J seba
 #SBATCH -N 1
-#SBATCH -n 8
+#SBATCH -n 4
 #SBATCH -o %x%j.o
 #SBATCH -e %x%j.e
 #SBATCH -p mrcs
 #SBATCH -w Zeta
 
 #tool_path
-BWA=/data/MRC1_data4/kysbbubbu/tools/bwa-0.7.17
-GATK=/data/MRC1_data4/kysbbubbu/tools/gatk-4.2.5.0
-QUALIMAP=/data/MRC1_data4/kysbbubbu/tools/qualimap_v2.2.1
-DB=/data/MRC1_data4/kysbbubbu/genomicdb
-FASTQC=/data/MRC1_data4/kysbbubbu/tools/FastQC_v0.11.9
-FASTQ_DIR=/data/MRC1_data4/kysbbubbu/Project_Rawdata/Seba_SRA
+BWA=/data/Delta_data4/kysbbubbu/tools/bwa-0.7.17
+GATK=/data/Delta_data4/kysbbubbu/tools/gatk-4.2.5.0
+QUALIMAP=/data/Delta_data4/kysbbubbu/tools/qualimap_v2.2.1
+DB=/data/Delta_data4/kysbbubbu/genomicdb
+FASTQ_DIR=./
 
 ##OUTPUT
 FASTQC_DIR=./01_FastQC
@@ -21,7 +20,7 @@ BAM_DIR=./02_BAM
 BAMQC_DIR=./03_BAMQC
 MT_DIR=./04_MT
 ANNO_DIR=./05_Annovar
-for i in 1N 2N 3N 4N 5N 6N 7N
+for i in SN03T
 do
 $GATK/gatk --java-options "-Xmx4g" FastqToSam -SM $i -F1 $FASTQ_DIR/$i\_1.fastq.gz -F2 $FASTQ_DIR/$i\_2.fastq.gz -RG $i -O $i\_unmapped.bam
 $BWA/bwa mem -t 8 $DB/human_g1k_v37.fasta $FASTQ_DIR/$i\_1.fastq.gz $FASTQ_DIR/$i\_2.fastq.gz > $i\.sam
@@ -36,5 +35,6 @@ $GATK/gatk --java-options "-Xmx4g" ApplyBQSR -R $DB/human_g1k_v37.fasta -I $i\.b
 rm $i\.bam $i\.bam.bai $i\_recal_data.table
 $GATK/gatk --java-options "-Xmx4g" GetPileupSummaries -R $DB/human_g1k_v37.fasta -I $BAM_DIR/$i\_b37.bam -O $MT_DIR/$i\_pileups.table -V $DB/af-only-gnomad.raw.sites.b37.vcf.gz --intervals $DB/SureselectV5.list
 $QUALIMAP/qualimap bamqc -bam $BAM_DIR/$i\_b37.bam -gff $DB/SureselectV5.bed -outdir $BAMQC_DIR/$i -c --java-mem-size=4G
+$GATK/gatk --java-options "-Xmx4g" HaplotypeCaller -R $DB/human_g1k_v37.fasta -I $BAM_DIR/$i\_b37.bam --intervals $DB/Sensus_v92_exome.list -ERC GVCF -O $MT_DIR/$i\_germline.vcf.gz
 done
 
